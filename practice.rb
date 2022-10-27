5ec8e061f64eab381f52c1056b0cc100155d28bb
Trina Nixon <TrinaNixon7@gmail.com>
2022-10-27 11:59:13 -0600

    practice examples

--- practice.rb
+++ practice.rb
@@ -19,3 +19,9 @@ end
 p "Before no_mutate method: #{a}"
 no_mutate(a)
 p "After no_mutate method: #{a}"
+
+def add_three(number)
+  number + 3
+end
+returned_value = add_three(4)
+puts returned_value
