feec36799ede86d9d140267b0f2046658314577d
Trina Nixon <TrinaNixon7@gmail.com>
2022-10-27 11:47:41 -0600

    practice example

--- practice.rb
+++ practice.rb
@@ -8,3 +8,14 @@ end
 p "Before mutate method: #{a}"
 mutate(a)
 p "After mutate method: #{a}"
+
+a = [1, 2, 3]
+
+# Example of a method definition that does not mutate the caller
+def no_mutate(array)
+  array.last
+end
+
+p "Before no_mutate method: #{a}"
+no_mutate(a)
+p "After no_mutate method: #{a}"
