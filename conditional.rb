d93c60208ce90c7a79dc0be7c20a09453ed5e860
Trina Nixon <TrinaNixon7@gmail.com>
2022-11-12 14:12:37 -0700

    practice problem

--- conditional.rb
+++ conditional.rb
@@ -1,11 +1,2 @@
-puts "Put in a number"
-a = gets.chomp.to_i
-
-if a == 3 
-  puts "a is 3"
-elsif a == 4 
-  puts "a is 4"
-else
-    puts "a is neither 3, nor 4"
-end 
-
+# Snippet 1
+'4' == 4 ? puts("TRUE") : puts("FALSE")
