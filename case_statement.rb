c09cac7119f9c8b95b063a893b974979ed500701
Trina Nixon <TrinaNixon7@gmail.com>
2022-11-12 14:09:26 -0700

    practice problem

--- case_statement.rb
+++ case_statement.rb
@@ -1,10 +1,18 @@
-def all_caps(string)
-    if string.length > 10
-      string.upcase
-    else    
-      string
-    end
-  end
+puts "Please enter a number between 0 and 100"
 
-  puts all_caps("dinosaurs are extinct")
-  puts all_caps("dinosaurs")
+def user_num(num)
+  if num >= 0 && num <= 50
+    puts "Number is between 0 and 50"
+  elsif num >= 51 && num <= 100
+    puts "Number is between 51 and 100"
+  else num > 100
+    puts  "Number is greater than 100"
+  end
+end 
+
+puts user_num(0)
+puts user_num(50)
+puts user_num(51)
+puts user_num(65)
+puts user_num(200)
+puts user_num(100)
