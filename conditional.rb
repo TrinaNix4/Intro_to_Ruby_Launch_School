8188b9d9ac215bd2ac450033797077ad9cfc9464
Trina Nixon <TrinaNixon7@gmail.com>
2022-11-12 14:21:51 -0700

    practice example

--- conditional.rb
+++ conditional.rb
@@ -1,7 +1,10 @@
-x = 8
-if ((x * 3)/2) == (4 + 4 - x - 3)
-  puts "Did you get it right?"
-else  
-  puts ("Did you?")
-end 
+y = 9
+x = 10
+if (x + 1) <= (y)
+  puts "Alright"
+elsif (x + 1) >= (y)
+  puts "Alright now!"
+else 
+  puts "Alrighty!"
+end
 
