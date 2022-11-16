be414be660dc73ad42c5326bbe8de2f7312cc37f
Trina Nixon <TrinaNixon7@gmail.com>
2022-11-15 17:44:58 -0700

    practice example

---
+++ practice.rb
@@ -0,0 +1,40 @@
+a = [1, 2, 3]
+
+# Example of a method definition that mutates its argument permanently
+def mutate(array)
+  array.pop
+end
+
+p "Before mutate method: #{a}"
+mutate(a)
+p "After mutate method: #{a}"
+
+a = [1, 2, 3]
+
+# Example of a method definition that does not mutate the caller
+def no_mutate(array)
+  array.last
+end
+
+p "Before no_mutate method: #{a}"
+no_mutate(a)
+p "After no_mutate method: #{a}"
+
+def add_three(number)
+  number + 3
+end
+returned_value = add_three(4)
+puts returned_value
+
+
+def greeting(name)
+  return "Hello, " + name
+end 
+
+puts greeting("Trina")
+
+def multiply(num1, num2)
+  num1 * num2
+end
+
+puts multiply(4,5)
