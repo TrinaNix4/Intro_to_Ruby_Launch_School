f97ee8f7dedd29979ed111c53dfcbe5c7c00e55f
Trina Nixon <TrinaNixon7@gmail.com>
2022-12-01 14:35:56 -0700

    practice example

---
+++ practice.rb
@@ -0,0 +1,17 @@
+top_five_bands = ["Puscifer", "Tool", "A Perfect Circle", "Chelsea Wolfe", "Interpol"]
+
+top_five_bands.each_with_index do |band, index | 
+  puts "#{index + 1}. #{band}"
+end
+
+
+arr = [2, 4, 5, 6, 7]
+new_arr = []
+
+arr.each do |n|
+  new_arr << n + 2
+end
+
+p arr
+p new_arr
+
