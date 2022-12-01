4d77f4f4cb772b54a39c73c845a73f2fc7dae7df
Trina Nixon <TrinaNixon7@gmail.com>
2022-12-01 12:03:36 -0700

    .each practice

--- practice.rb
+++ practice.rb
@@ -3,3 +3,14 @@ top_five_bands = ["Puscifer", "Tool", "A Perfect Circle", "Chelsea Wolfe", "Inte
 top_five_bands.each_with_index do |band, index | 
   puts "#{index + 1}. #{band}"
 end
+
+
+arr = [1, 2, 3, 4]
+new_arr = []
+
+arr.each do |n|
+  new_arr << n + 2
+end
+
+p arr
+p new_arr
