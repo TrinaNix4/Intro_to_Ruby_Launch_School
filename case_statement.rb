25bf0eac293a371efef68712af08960683bfa495
Trina Nixon <TrinaNixon7@gmail.com>
2022-12-07 13:46:47 -0700

    select method practice example

--- case_statement.rb
+++ case_statement.rb
@@ -1,7 +1,5 @@
 array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-array.each do |number|
-  puts number 
-end 
+new_array = array.select{ |number| number % 2 == 0 }
 
-array.each{|number| puts number }
+print new_array
