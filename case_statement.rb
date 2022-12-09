150a3dc51943eefbc44a558ad4d5f24026b5a32f
Trina Nixon <TrinaNixon7@gmail.com>
2022-12-08 22:47:26 -0700

    practice example

--- case_statement.rb
+++ case_statement.rb
@@ -3,3 +3,7 @@ array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 new_array = array.select{ |number| number % 2 == 0 }
 
 print new_array
+
+new_array = array.select{ |number| number % 2 == 0 }
+
+print new_array
