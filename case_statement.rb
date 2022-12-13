920c21861cfc2f3b390e69a496a1ee61c008b717
Trina Nixon <TrinaNixon7@gmail.com>
2022-12-13 12:20:58 -0700

    practice example

--- case_statement.rb
+++ case_statement.rb
@@ -1,12 +1,14 @@
-h = {a:1, b:2, c:3, d:4}
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
+            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
 
-h[:b]
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
 
-h[:e] = 5
 
-h.delete_if{ |k, v| v < 3.5}
+contacts["Joe Smith"][:email] = contact_data[0][0]
+contacts["Joe Smith"][:address] = contact_data[0][1]
+contacts["Joe Smith"][:phone] = contact_data[0][2]
+contacts["Sally Johnson"][:email] = contact_data[1][0]
+contacts["Sally Johnson"][:address] = contact_data[1][1]
+contacts["Sally Johnson"][:phone] = contact_data[1][2]
 
-h.delete_if do |k, v|
-  v < 3.5
-end 
-
+p contacts 
