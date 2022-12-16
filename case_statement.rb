be91237c6811f55e68d7b80a09ae22c4d860c100
Trina Nixon <TrinaNixon7@gmail.com>
2022-12-16 12:14:06 -0700

    practice example

--- case_statement.rb
+++ case_statement.rb
@@ -1,6 +1,4 @@
-arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
-
-arr.delete_if{ |string| string.start_with?("s", "w") }
-
-p arr
-
+result = while true
+  break
+end
+p result
