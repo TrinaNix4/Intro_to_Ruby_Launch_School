273caef3885a3dc7dcad7d8938d0966db3c16d8c
Trina Nixon <TrinaNixon7@gmail.com>
2022-12-22 13:17:59 -0700

    loop practice

--- case_statement.rb
+++ case_statement.rb
@@ -1,7 +1,5 @@
-iterations = 1
-
 loop do
-  puts "Number of iterations = #{iterations}"
-  iterations += 1
-  break if iterations > 5
+  puts 'Should I stop looping?'
+  answer = gets.chomp
+  break if answer = 'yes'
 end
