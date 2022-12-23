7d068f55c18fa00cf8ff43daae5506b3d66df889
Trina Nixon <TrinaNixon7@gmail.com>
2022-12-22 19:08:00 -0700

    loop practice

--- case_statement.rb
+++ case_statement.rb
@@ -1,8 +1,8 @@
 say_hello = true
 count = 0
 
-while say_hello
-  puts 'Hello!'
-  count += 1
-  say_hello = false if count == 5
-end
+while say_hello 
+    puts "helloj!"
+    count += 1
+    say_hello = false if count == 5
+end 
