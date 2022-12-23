d87df1c81ab393985f83e7a8d34da9c67fd4aa2d
Trina Nixon <TrinaNixon7@gmail.com>
2022-12-23 15:47:42 -0700

    loop practice

--- case_statement.rb
+++ case_statement.rb
@@ -1,6 +1,11 @@
-friends = ['Sarah', 'John', 'Hannah', 'Dave']
+count = 1
 
-for friend in friends
-  puts "hello, #{friend}"
-end 
-
+loop do
+  if count.even?
+    puts "#{count} is even"
+  else
+    puts "#{count} is odd"
+  end
+    break if count ==5  
+  count += 1
+end
