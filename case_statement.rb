7300d477eb1bbf02b8370d96792099d0f9894455
Trina Nixon <TrinaNixon7@gmail.com>
2022-12-28 16:46:58 -0700

    practice

--- case_statement.rb
+++ case_statement.rb
@@ -1,10 +1,3 @@
-def greeting
-  puts 'Hello!'
-end
-
-number_of_greetings = 2
-
-while number_of_greetings > 0
-  greeting
-  number_of_greetings -= 1
-end
+puts ">> Type anything you want:"
+text = gets
+puts text
