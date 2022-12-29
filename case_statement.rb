bce036989cbce10f4ba72404b19acd933da56f06
Trina Nixon <TrinaNixon7@gmail.com>
2022-12-28 20:45:37 -0700

    user input practice

--- case_statement.rb
+++ case_statement.rb
@@ -1,3 +1,7 @@
-puts ">> Type anything you want:"
-text = gets
-puts text
+
+puts '>> imagine if you were this old
+  ?'
+age_in_years = gets
+age_in_months = 12 * age_in_years.to_i
+puts "You are #{age_in_months} yrs.
+ old."
