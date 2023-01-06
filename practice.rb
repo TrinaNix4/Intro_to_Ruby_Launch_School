6d1b7ca7896da3e301bfff5ad0f84edccef3e449
Trina Nixon <TrinaNixon7@gmail.com>
2023-01-06 10:35:14 -0700

    user input practice example

--- practice.rb
+++ practice.rb
@@ -1,7 +1,8 @@
-
-puts "do you want me to print something? y/n"
-
-answer = gets.chomp
-
-puts 'something' if answer == 'y'
-
+choice = nil
+loop do
+  puts '>> Do you want me to print something? (y/n)'
+  choice = gets.chomp.downcase
+  break if %w(y n).include?(choice)
+  puts '>> Invalid input! Please enter y or n'
+end
+puts 'something' if choice == 'y'
