c49833427aae50a0d5b7e932a6b56b7defb739ed
Trina Nixon <TrinaNixon7@gmail.com>
2023-01-11 09:52:06 -0700

    user input example

--- practice.rb
+++ practice.rb
@@ -1,5 +1,14 @@
-puts How many output lines do you want? Please enter a number 3 or higher.
-  
-  number = gets.chomp_to_i
+puts 'how many lines would you like to print. Please enter a number greater than or equal to 3'
+input = nil
 
-  
+loop do 
+  puts "How many output lines do you want?  Enter a number greater or equal to 3"
+input = gets.to_i
+break if input >= 3
+puts "That's not enough lines"
+  end 
+
+while input > 0
+  puts "Launch School is the best!"
+input -= 1
+end 
