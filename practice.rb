7654ad32a18660914dafb1ffbd86b515012b2f67
Trina Nixon <TrinaNixon7@gmail.com>
2023-01-11 09:43:44 -0700

    practice example

--- practice.rb
+++ practice.rb
@@ -1,8 +1,5 @@
-choice = nil
-loop do
-  puts '>> Do you want me to print something? (y/n)'
-  choice = gets.chomp.downcase
-  break if %w(y n).include?(choice)
-  puts '>> Invalid input! Please enter y or n'
-end
-puts 'something' if choice == 'y'
+puts How many output lines do you want? Please enter a number 3 or higher.
+  
+  number = gets.chomp_to_i
+
+  
