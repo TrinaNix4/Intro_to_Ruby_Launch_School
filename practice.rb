e4350d9343e756745219396142f86b98a201f2e3
Trina Nixon <TrinaNixon7@gmail.com>
2023-01-11 14:49:05 -0700

    user input practice

--- practice.rb
+++ practice.rb
@@ -1,12 +1,27 @@
-number_of_lines = nil
-loop do
-  puts '>> How many output lines do you want? Enter a number >= 3:'
-  number_of_lines = gets.to_i
-  break if number_of_lines >= 3
-  puts ">> That's not enough lines."
-end
 
-while number_of_lines > 0
-  puts 'Launch School is the best!'
-  number_of_lines -= 1
-end
+
+
+
+
+loop do
+  puts "how many output lines do you want?" \ 'enter a number greater than or equal to 3'
+
+  input_string = gets.chomp.downcase
+
+break if input_string = 'q'
+
+number_of_lines = input_string.to_i
+
+break if number of lines >= 3
+
+puts "not enough lines"
+  end
+
+  break if input_string == 'q'
+
+  while number > 0
+    puts "launch school is the best"
+    number_of_lines -= 1
+  end 
+
+end 
