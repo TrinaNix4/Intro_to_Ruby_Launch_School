09de7a8f07c13a997e0411f6e6f6c008b8168127
Trina Nixon <TrinaNixon7@gmail.com>
2023-01-11 11:03:00 -0700

    user input practice

--- practice.rb
+++ practice.rb
@@ -1,11 +1,16 @@
 
 pw = 'defiefnei'
+username = 'admin'
 
 loop do
-puts "Please enter password to proceed"
-password_try = gets.chomp
-break if password_try == pw
-puts "Invalid password. Please try again."
+  puts 'Please enter username'
+  user_name = gets.chomp
+
+  puts "Please enter password"
+  password_try = gets.chomp
+
+break if user_name == username && password_try == pw
+puts "Authorization failed. Please try again."
 end
 
 puts "Welcome!"
