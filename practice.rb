bdcfbb3b62498fa2e9d43a2665ea02f92209d0ad
Trina Nixon <TrinaNixon7@gmail.com>
2023-01-11 10:01:26 -0700

    user input practice

--- practice.rb
+++ practice.rb
@@ -1,14 +1,11 @@
-puts 'how many lines would you like to print. Please enter a number greater than or equal to 3'
-input = nil
 
-loop do 
-  puts "How many output lines do you want?  Enter a number greater or equal to 3"
-input = gets.to_i
-break if input >= 3
-puts "That's not enough lines"
-  end 
+pw = 'defiefnei'
 
-while input > 0
-  puts "Launch School is the best!"
-input -= 1
-end 
+loop do
+puts "Please enter password to proceed"
+password_try = gets.chomp
+break if password_try == pw
+puts "Invalid password. Please try again."
+end
+
+puts "Welcome!"
