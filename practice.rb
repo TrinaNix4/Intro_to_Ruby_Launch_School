22bdbdf01ec73705d11e1419850c721bf32f2df2
Trina Nixon <TrinaNixon7@gmail.com>
2023-01-12 11:41:03 -0700

    user input practice

--- practice.rb
+++ practice.rb
@@ -1,27 +1,35 @@
+def valid_number?(number_string)
+  number_string.to_i.to_s == number_string && number_string.to_i != 0
+end
 
+#validating whether the input is a valid number
+#if not valid, it continues the loop
+def read_number
+  loop do
+  puts "Please enter a positive or negative integer:"
+  number = gets.chomp
+  return number.to_i if valid_number?(number)
+  puts "invalid input  only non-zero integers are allowed"
+  end
+end
 
-
-
+first_number = nil
+second_number = nil 
 
 loop do
-  puts "how many output lines do you want?" \ 'enter a number greater than or equal to 3'
+  first_number = read_number
+  second_number = read_number
 
-  input_string = gets.chomp.downcase
+#verify whether one of these numbers is greater than zero and one is less than zero; order doesn't matter; one should be positive and one negative
 
-break if input_string = 'q'
 
-number_of_lines = input_string.to_i
+# if the product is less than zero, we know we had one positive and one negative so....
+  break if first_number * second_number < 0
+    puts "Sorry, one integer must be positive and one must be negative"
+    puts "Please start over."
+end
 
-break if number of lines >= 3
+sum = first_number + second_number
 
-puts "not enough lines"
-  end
+puts "#{first_number} + #{second_number} = #{sum}"
 
-  break if input_string == 'q'
-
-  while number > 0
-    puts "launch school is the best"
-    number_of_lines -= 1
-  end 
-
-end 
