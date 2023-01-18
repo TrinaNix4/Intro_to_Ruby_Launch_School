a4912cf54d8125baed7f45765ec7a4b62c4c85d9
Trina Nixon <TrinaNixon7@gmail.com>
2023-01-17 21:47:09 -0700

    practice example

--- practice.rb
+++ practice.rb
@@ -1,5 +1,12 @@
-
-def running_sum(nums)
-  sum = 0
-  nums.map { |num| sum += num }
+def findLeastNumOfUniqueInts(arr, k)
+  count = arr.each_with_object(Hash.new(0)) { |x, hsh| hsh[x] += 1 }
+  count = count.sort_by { |_, v| v }
+  remove = 0
+  count.each do |_, v|
+    remove += v
+    if remove > k
+      return count.length - (remove - v)
+    end
+  end
+  0
 end
