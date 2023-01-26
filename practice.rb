07b054f8f5e25d91cd27c4ef58abd60502f5d2dd
Trina Nixon <TrinaNixon7@gmail.com>
2023-01-26 12:21:57 -0700

    conditional practice

--- practice.rb
+++ practice.rb
@@ -1,6 +1,5 @@
-sun = ['visible', 'hidden'].sample
+sun = 'visible'
 
-if sun == 'visible'
-  puts "The sun is so bright!"
-end 
-
+unless sun == 'visible'
+  puts "the clouds are blocking the sun!"
+end
