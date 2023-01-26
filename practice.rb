30a90a9318edfde881f59f8b9d3a95ec6dabee67
Trina Nixon <TrinaNixon7@gmail.com>
2023-01-26 10:11:06 -0700

    return practice example

--- practice.rb
+++ practice.rb
@@ -1,12 +1,5 @@
-def findLeastNumOfUniqueInts(arr, k)
-  count = arr.each_with_object(Hash.new(0)) { |x, hsh| hsh[x] += 1 }
-  count = count.sort_by { |_, v| v }
-  remove = 0
-  count.each do |_, v|
-    remove += v
-    if remove > k
-      return count.length - (remove - v)
-    end
-  end
-  0
+def meal
+  'Evening'
 end
+
+puts meal
