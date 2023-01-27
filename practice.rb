d2b9b583c9d2e9b1806bb8ffaad761d86a13c1c5
Trina Nixon <TrinaNixon7@gmail.com>
2023-01-27 16:36:54 -0700

    conditional practice

--- practice.rb
+++ practice.rb
@@ -1,8 +1,5 @@
-number = rand(10)
-
-if number == 5
-  puts '5 is a cool number'
-else
-  puts 'other numbers are cool too!'
+case stoplight
+when 'green'  then puts 'Go!'
+when 'yellow' then puts 'Slow down!'
+else               puts 'Stop!'
 end
-
