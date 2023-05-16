df486762ebad18b0b1eff4de5805df179179844d
Trina Nixon <TrinaNixon7@gmail.com>
2023-05-16 11:17:58 -0600

    lecture practice

--- practice.rb
+++ practice.rb
@@ -16,8 +16,23 @@ puts state
 
 words = 'car human elephant airplane'
 
-words.split(' ').each do |words|
+words.split(' ').each do |word|
   puts words + 's'
 
 end 
+##################
+
+colors = "blue boredom yellow"
+
+
+puts colors.include?('red')
+
+
+########
+
+pets = ['cat', 'dog', 'fish', 'lizard']
+
+my_pet = pets[2]
+
+puts("I have a pet #{my_pet}")
 
