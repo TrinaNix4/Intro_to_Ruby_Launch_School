3f62f7d3b886c5c19dea7ebb1c0651291f584ce6
Trina Nixon <TrinaNixon7@gmail.com>
2023-05-16 11:57:16 -0600

    lecture practice

--- practice.rb
+++ practice.rb
@@ -32,7 +32,8 @@ puts colors.include?('red')
 
 pets = ['cat', 'dog', 'fish', 'lizard']
 
-my_pet = pets[2]
+my_pets = pets[2..3]
+my_pets.pop
 
-puts("I have a pet #{my_pet}")
+puts"I have a pet #{my_pets[0]}"
 
